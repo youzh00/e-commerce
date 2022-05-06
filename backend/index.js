cd7d2b7fa5a3dcd1650b874b6441fbd3a0c63708
Youssef Zahi <youssef.zahi2016@gmail.com>
2022-05-06 01:15:14 +0000

    fetching products from database

--- backend/index.js
+++ backend/index.js
@@ -9,16 +9,18 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 const connect = require("./DB_Config/dataBase");
 const colors = require("colors");
+const productsRouter = require("./routes/productsRoutes");
 
 //!-------------Code part------------------//
 connect();
+app.use(productsRouter);
 app.get("/", (req, res) => {
   res.send("hahahhahaa");
 });
 
-app.get("/api/products", (req, res) => {
-  res.send(products);
-});
+// app.get("/api/products", (req, res) => {
+//   res.send(products);
+// });
 
 app.get("/api/products/:id", (req, res) => {
   const product = products.find((p) => p._id === req.params.id);
---
+++ backend/routes/productsRoutes.js
@@ -0,0 +1,34 @@
+const print = (args) => console.log(args);
+//!--------------Requirement---------------//
+const mongoose = require("mongoose");
+const Product = require("../models/productModel");
+const express = require("express");
+const colors = require("colors");
+const router = express.Router();
+const asyncHandler = require("express-async-handler");
+//!-------------Code part------------------//
+//get products from the database
+router.get(
+  "/products",
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.send(products);
+  })
+);
+
+//get only one product from the database
+router.get(
+  "/products/:id",
+  asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    print("hhahahhah");
+    const product = await Product.findById(id);
+    if (product) {
+      res.send(product);
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  })
+);
+
+module.exports = router;
