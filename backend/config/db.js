5703c887717f87c8f4d6da2951c24281482f858b
Elab4d1 <hassanflv@gmail.com>
2022-05-11 11:42:22 +0100

    refactor connectDB function code

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,8 @@
 const print = (args) => console.log(args);
 //!-------------Requirement--------------//
-const { log } = require("console");
 const mongoose = require("mongoose");
-const colors = require("colors");
 
-const connect = async () => {
-  try {
-    const cnxDb = await mongoose.connect(process.env.MongoDB_URL, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    });
-    print(`MongoDb Connected: ${cnxDb.connection.host}`.yellow);
-  } catch (error) {
-    print(error.red.bold);
-  }
+const connectDB = async () => {
+  return mongoose.connect(process.env.MONGODB_URL);
 };
-module.exports = connect;
+module.exports = connectDB;
