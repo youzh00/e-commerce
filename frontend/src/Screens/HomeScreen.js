88f7c0b7dc254b92815ad2fbd4c82fbbd6081c6d
Elab4d1 <hassanflv@gmail.com>
2022-05-11 11:36:45 +0100

    refactor home screen 2

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -35,12 +35,15 @@ export const HomeScreen = () => {
   }
 
   return (
-    <Row>
-      {products.map((product) => (
-        <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-          <Product product={product} />
-        </Col>
-      ))}
-    </Row>
+    <>
+      <h1> Latest Products </h1>
+      <Row>
+        {products.map((product) => (
+          <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
   );
 };
