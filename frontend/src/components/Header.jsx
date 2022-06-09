74b121ec36f3b103255550a96f4fb0ec724f629b
Youssef Zahi <youssef.zahi2016@gmail.com>
2022-06-09 22:15:12 +0100

    admin user ,product, orders list added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -56,6 +56,21 @@ const Header = () => {
                   </Nav.Link>
                 </LinkContainer>
               )}
+              {
+                userInfo && userInfo.isAdmin && (
+                  <NavDropdown title='admin' id="admin">
+                  <LinkContainer to="admin/userslist">
+                    <NavDropdown.Item>Users</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to="admin/productlist">
+                    <NavDropdown.Item>Products</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to="admin/orderlist">
+                    <NavDropdown.Item>Orders</NavDropdown.Item>
+                  </LinkContainer>
+                </NavDropdown>
+                )
+              }
             </Nav>
           </Navbar.Collapse>
         </Container>
