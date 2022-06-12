671d65643de678d692104d4a9093dade601efb4f
Youssef Zahi <youssef.zahi2016@gmail.com>
2022-06-12 14:21:08 +0100

    update order to delivered ,order router-backend

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -7,6 +7,7 @@ const { getOrderById } = require("../controllers/orderController");
 const { updateOrderToPaid } = require("../controllers/orderController");
 const { getUserOrders } = require("../controllers/orderController");
 const { getAllOrders } = require("../controllers/orderController");
+const { updateOrderToDelivered } = require("../controllers/orderController");
 
 const { protect, isAdmin } = require("../middleware/authMiddleware");
 
@@ -16,6 +17,7 @@ router
   .get(protect, isAdmin, getAllOrders);
 router.route("/:id").get(protect, getOrderById);
 router.route("/:id/pay").put(protect, updateOrderToPaid);
+router.route("/:id/delivered").put(protect, isAdmin, updateOrderToDelivered);
 router.route("/myorders").get(protect, getUserOrders);
 
 module.exports = router;
